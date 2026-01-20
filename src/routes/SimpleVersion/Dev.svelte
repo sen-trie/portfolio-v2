2953051d7b551b5ce980a21bb35e541b21cc97ca
ayaya-pc <122269172+sen-trie@users.noreply.github.com>
2026-01-20 20:32:39 +0800

    Simple-Dev: Add dates

--- src/routes/SimpleVersion/Dev.svelte
+++ src/routes/SimpleVersion/Dev.svelte
@@ -1,79 +1,32 @@
 <script>
 	import { getContext } from 'svelte';
 	import { fly } from 'svelte/transition';
+	import devItems from '$lib/db/dev-items.json';
 
 	const images = getContext('images');
-
-	const devItems = [
-		{
-			name: 'Basil Club - Recipe Site',
-			desc: 'A personal recipe collection. Adapted recipes from well-known sources, organized for easy reference while baking.',
-			tech: ['Svelte', 'JavaScript'],
-			repo: 'https://github.com/sen-trie/basil-main',
-			site: 'https://www.basilclub.cafe/',
-			image: 'proj7',
-			imageAlt: 'Basil Club Recipe Project'
-		},
-		{
-			name: 'Basil Club - 3D Webapp',
-			desc: 'An interactive 3D café. Contains multiple interactables and references, inspired by personal experiences.',
-			tech: ['Threlte', 'Three'],
-			repo: 'https://github.com/sen-trie/basil-club',
-			site: 'https://explore.basilclub.cafe/',
-			image: 'proj6',
-			imageAlt: 'Basil Club 3D Project'
-		},
-		{
-			name: 'BrushHub',
-			desc: 'A frontend showcase aimed at demonstrating the concept and user experience of an artwork commissioning platform.',
-			tech: ['Svelte', 'Tailwind', 'TypeScript'],
-			repo: 'https://github.com/sen-trie/brushhub',
-			site: 'https://sen-trie.github.io/brushhub/',
-			image: 'proj5',
-			imageAlt: 'BrushHub Project'
-		},
-		{
-			name: 'nahidaQuest!',
-			desc: 'An incremental game based on the popular hit Genshin Impact, built entirely from scratch. Playable on modern desktop and mobile browsers.',
-			tech: ['HTML', 'CSS', 'JavaScript'],
-			repo: 'https://github.com/sen-trie/nahidaQuest',
-			site: 'https://nahidaquest.com/',
-			image: 'proj1',
-			imageAlt: 'nahidaQuest Project'
-		},
-		{
-			name: '2D Heat Diffusion',
-			desc: 'Simulates heat transfer by conduction through a two dimensional plate. Computationally solves the heat partial differential equation and visualizes the solution as a heatmap.',
-			tech: ['MATLAB'],
-			repo: 'https://github.com/sen-trie/2d-heat-diffusion',
-			site: null,
-			image: 'proj2',
-			imageAlt: 'Matlab Project'
-		},
-		{
-			name: 'The Lair',
-			desc: 'A fictional online storefront set in a world populated by vampires.',
-			tech: ['React', 'JavaScript'],
-			repo: 'https://github.com/sen-trie/shopping-cart',
-			site: 'https://sen-trie.github.io/shopping-cart/',
-			image: 'proj3',
-			imageAlt: 'Storefront Project'
-		}
-	];
 </script>
 
 <h1 class="content-title">Projects</h1>
 {#each devItems as project}
 	<div class="dev-block">
 		<div class="block-text">
-			<h2 class="block-name">{project.name}</h2>
-			<div class="block-ingredients">
-				{#each project.tech as tech}
-					<span class="flexbox">
-						<img class="tech-svg" src={images[`dev/${tech.toLowerCase()}.svg`]} alt="{tech} logo" />
-						{tech}
-					</span>
-				{/each}
+			<div class="block-top">
+				<div class="flexbox block-title">
+					<h2 class="block-name">{project.name}</h2>
+					<h4><i>{project.year}</i></h4>
+				</div>
+				<div class="block-ingredients">
+					{#each project.tech as tech}
+						<span class="flexbox">
+							<img
+								class="tech-svg"
+								src={images[`dev/${tech.toLowerCase()}.svg`]}
+								alt="{tech} logo"
+							/>
+							{tech}
+						</span>
+					{/each}
+				</div>
 			</div>
 			<h3 class="block-desc">{project.desc}</h3>
 		</div>
@@ -117,9 +70,7 @@
 		flex-direction: row;
 		justify-content: flex-start;
 		height: fit-content;
-		border-bottom: 2px solid var(--text-main);
-		padding-bottom: 12px;
-		margin: 6px 0 8px;
+		margin-top: 6px;
 
 		.tech-svg {
 			height: 24px;
@@ -140,6 +91,19 @@
 	.block-text {
 		width: 50%;
 		padding: 0.1rem 0 0 0;
+		margin-bottom: 6px;
+
+		.block-title {
+			justify-content: space-between;
+		}
+
+		.block-top {
+			border-bottom: 2px solid var(--text-main);
+			padding-bottom: 12px;
+			justify-content: space-between;
+			align-items: flex-start;
+			width: 100%;
+		}
 	}
 
 	.block-pic {
